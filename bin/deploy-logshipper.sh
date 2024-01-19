5dc60cacbb55c686f3cf28667d51927dfd284dac
Amy Farrell <amy.farrell@gsa.gov>
2024-01-19 15:31:43 -0800

    Specify two instances and rolling strategy when deploying

--- bin/deploy-logshipper.sh
+++ bin/deploy-logshipper.sh
@@ -2,7 +2,7 @@
 
 # Always clear out the old cg-logshipper directory. We need to do this
 # if we've changed the branch/tag we want to deploy or if we've removed some files
-# from project_conf.
+# from project_conf; it's easiest to just do it every time.
 if [ -d "cg-logshipper" ]; then
    rm -rf cg-logshipper
 fi
@@ -10,8 +10,8 @@ fi
 # Clone cg-logshipper at the specified branch/tag
 git clone --depth 1 --branch usagov-1.0 git@github.com:usagov/cg-logshipper.git
 
-cp -rp project_conf cg-logshipper;
+cp -rp project_conf cg-logshipper
 
-cd cg-logshipper;
+cd cg-logshipper
 
-cf push;
+cf push --instances 2 --strategy rolling
