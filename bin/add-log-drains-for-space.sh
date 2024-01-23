3abe803cb7ab11146de354a813515c61dbc2ffc4
Amy Farrell <amy.farrell@gsa.gov>
2024-01-22 17:02:22 -0800

    Make add-log-drains-for-space idempotent, sort of

--- bin/add-log-drains-for-space.sh
+++ bin/add-log-drains-for-space.sh
@@ -6,8 +6,15 @@
 set -e
 set -o pipefail
 
-echo "Creating log-shipper-drain service"
-cf create-user-provided-service log-shipper-drain -l "https://${HTTP_USER}:${HTTP_PASS}@usagov-tools-logshipper.app.cloud.gov/?drain-type=all"
+SERVICE_EXISTS=`cf service log-shipper-drain --guid`
+
+if [ "$SERVICE_EXISTS" = "FAILED" ]; then
+    echo "Creating log-shipper-drain service"
+    cf create-user-provided-service log-shipper-drain -l "https://${HTTP_USER}:${HTTP_PASS}@usagov-tools-logshipper.app.cloud.gov/?drain-type=all"
+else
+    echo "Service log-shipper-drain already exists."
+fi
+
 
 applist=$(cf apps | tail -n +4 | awk '{print $1}')
 
