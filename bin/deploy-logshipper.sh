7e3bd3b0ea47f908e4bc25eedeb78b6683626b14
Amy Farrell <amy.farrell@gsa.gov>
2024-01-31 16:18:32 -0800

    Add a status file to the deployed container

--- bin/deploy-logshipper.sh
+++ bin/deploy-logshipper.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+CONTAINERTAG=${1:-"/no pipeline number/"}
+
+# Get our branch and commit has for the status file:
+USAGOV_BRANCH=$(git symbolic-ref --short HEAD 2>/dev/null || echo "")
+USAGOV_COMMIT=$(git log -1 --pretty=format:"%H")
+
 # Always clear out the old cg-logshipper directory. We need to do this
 # if we've changed the branch/tag we want to deploy or if we've removed some files
 # from project_conf; it's easiest to just do it every time.
@@ -10,8 +16,19 @@ fi
 # Clone cg-logshipper at the specified branch/tag
 git clone --depth 1 --branch usagov-1.0 git@github.com:usagov/cg-logshipper.git
 
+# Copy in our own custom config
 cp -rp project_conf cg-logshipper
 
 cd cg-logshipper
 
+# Write a status file we can inspect if needed:
+echo "USAGov log-shipper deployment version:" >> ./DEPLOYED_VERSION.txt
+echo "    built:" $(date) >> ./DEPLOYED_VERSION.txt
+echo "    usagov-logshipper branch:" $USAGOV_BRANCH >> ./DEPLOYED_VERSION.txt
+echo "    usagov-logshipper commit:" $USAGOV_COMMIT >> ./DEPLOYED_VERSION.txt
+echo "    cg-logshipper branch:" $(git symbolic-ref --short HEAD 2>/dev/null || echo "") >> ./DEPLOYED_VERSION.txt
+echo "    cg-logshipper commit:" $(git log -1 --pretty=format:"%H") >> ./DEPLOYED_VERSION.txt
+echo "    containertag:" $CONTAINERTAG >> ./DEPLOYED_VERSION.txt
+
+# And push the app from the cg-logshipper directory
 cf push log-shipper --instances 2 --strategy rolling
