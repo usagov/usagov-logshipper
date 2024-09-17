a2689f67323fe5c3b671b2774487acba97487a11
Jacob Yeager <jacobaaronyeager@gmail.com>
2024-09-17 12:32:59 -0400

    simplifying

--- project_conf/scripts/parse_modsecurity_keys.lua
+++ project_conf/scripts/parse_modsecurity_keys.lua
@@ -1,26 +1,29 @@
 -- Gets modsecurity record and returns a record with a json string of the modsecurity attributes.
 
-local modsecurity_attributes_modify_string = function (orig_string)
-  local result = {}
-  local isFirstIteration = true
+local modsecurity_attributes_json_string = function (orig_string)
+  -- Extract the ModSecurity text from the message
   local modsecurity_text = orig_string:match("ModSecurity:(.-)%[")
-  for match in orig_string:gmatch("%[(.-)%]") do
-    if isFirstIteration then
-      key = match
-      value = modsecurity_text
-      isFirstIteration = false
-      value = value:gsub("^%s*(.-)%s*$", "%1")
-      formattedKey = "\"" .. key .. "\":\"" .. value .. "\""
+
+  -- Find text within square brackets and store in an array
+  local attributes = {}
+  table.insert(attributes, '"message":' .. modsecurity_text:gsub("^%s*(.-)%s*$", "%1"))
+  for attribute in string.gmatch(orig_string, "%[(.-)%]") do
+
+    -- Split attribute by whitespace
+    local key, value = attribute:match("(%S+)%s+(.*)")
+    if key and value then
+      attribute = '"' .. key .. '":' .. value:gsub("^%s*(.-)%s*$", "%1")
     else
-      key, value = match:match("(%S+)%s+(%S+)")
-      key = key:gsub("\"", "")
-      value = value:gsub("^%s*(.-)%s*$", "%1"):gsub("\"", "")
-      formattedKey = "\"" .. key .. "\":\"" .. value .. "\""
+      attribute = '"level":"' .. attribute:gsub("^%s*(.-)%s*$", "%1") .. '"'
     end
-      table.insert(result, formattedKey)
+    table.insert(attributes, attribute)
   end
 
-  final_string = table.concat(result, ", ")
+  -- Concatenate attributes into a JSON string
+  local final_string = table.concat(attributes, ",")
+
+  -- wrap modsecurity field in brackets to make it parsable json
+  -- local final_string = "{" .. final_string .. "}"
   return final_string
 end
 
@@ -30,9 +33,9 @@ end
 -- which expects it to be defined in this way)
 function parse_modsecurity_keys (_, timestamp, record) --luacheck: ignore
   if (record["message"] ~= nil and string.find(record["message"], "ModSecurity")) then
-    record["modsecurity"] = modsecurity_attributes_modify_string(record["message"])
+    record["modsecurity"] = modsecurity_attributes_json_string(record["message"])
   end
-    -- 2 leaves timestamp unchanged
+   -- 2 leaves timestamp unchanged
   return 2, timestamp, record
 end
 
@@ -69,6 +72,6 @@ end
 --   ["timestamp"]=1724059377936
 -- }
 
--- local output = modsecurity_attributes_json_string(input['message'])
+-- local _,timestamp,output = parse_modsecurity_keys("_","2/2/22", input)
 
--- print(output)
+-- print(output['modsecurity'])
