e66b0737de52bff40f9965892cdea4ee584b0844
Jacob Yeager <jacobaaronyeager@gmail.com>
2024-11-20 16:17:10 -0500

    modsecurity-parsing: Fix modsecurity parsing script

--- project_conf/scripts/parse_modsecurity_keys.lua
+++ project_conf/scripts/parse_modsecurity_keys.lua
@@ -1,39 +1,65 @@
 -- Gets modsecurity record and returns a record with a json string of the modsecurity attributes.
 
-local modsecurity_attributes_json_string = function (orig_string)
-  local final_string = "test"
-  local message, body, client, server, request, host =
-    orig_string:match("ModSecurity:(.-)%[(.-)%], client: (.-), server: (.-), request: (.-), host: (.-)$")
+-- This function is used to extract the main ModSecurity message from the message field
+function extract_modsecurity_message_string(s)
+  local startIndex = s:find("ModSecurity")
+  if startIndex then
+      local bracketIndex = s:find("%[", startIndex)
 
-  local attributes = {}
-  table.insert(attributes, '"message":"' .. message:gsub("^%s*(.-)%s*$", "%1"):gsub('"', '\\"') .. '"')
-  table.insert(attributes, '"client":"' .. client:gsub("^%s*(.-)%s*$", "%1"):gsub('"', '\\"') .. '"')
-  table.insert(attributes, '"server":"' .. server:gsub("^%s*(.-)%s*$", "%1"):gsub('"', '\\"') .. '"')
-  table.insert(attributes, '"request":' .. request:gsub("^%s*(.-)%s*$", "%1"):gsub('"', '\\"'))
-  table.insert(attributes, '"host":' .. host:gsub("^%s*(.-)%s*$", "%1"):gsub('"', '\\"'))
-
-  for attribute in body do
-    -- Split attribute by whitespace
-    local key, value = attribute:match("(%S+)%s+(.*)")
-    if key and value then
-      if key == "client" then
-        value = '"' .. value:gsub("^%s*(.-)%s*$", "%1") .. '"'
+      if bracketIndex then
+          return s:sub(startIndex, bracketIndex - 1)
       else
-        value = value:gsub("^%s*(.-)%s*$", "%1")
+          return '"message":"' .. s:sub(startIndex) .. '"'
       end
-      attribute = '"' .. key .. '":' .. value:gsub('"', '\\"')
-    else
-      attribute = '"level":"' .. attribute:gsub("^%s*(.-)%s*$", "%1"):gsub('"', '\\"') .. '"'
-    end
-    table.insert(attributes, attribute)
+  else
+      return '"message":"none"'
   end
+end
+
+-- This function is used to extract the data from the bracketed sections of the message field
+function extract_bracketed_data(s)
+  local pattern = "%[(.-)%]"
+  local matches = {}
+  for match in s:gmatch(pattern) do
+      local key, value = match:match("([^ ]+) (.+)")
+      if key then
+          table.insert(matches, '"' .. key .. '":"' .. (value or "") .. '"')
+      else
+          -- the only time there is no key is when the match is the level
+          table.insert(matches, '"level":"' .. (match or "") .. '"')
+      end
+  end
+  return matches
+end
+
+-- This function is used to extract the data from the final four unbracketed attributes of the message field
+function extract_trailing_data(s)
+  local pattern = ", (%w+): (.+)"
+  local matches = {}
+  for key, value in s:gmatch(pattern) do
+      table.insert(matches, '"' .. key .. '":"' .. value .. '"')
+  end
+  return matches
+end
+
+local function tableMerge(result, ...)
+  for _, t in ipairs({...}) do
+    for _, v in ipairs(t) do
+      table.insert(result, v)
+    end
+  end
+end
+
+local modsecurity_attributes_json_string = function (orig_string)
+  local attributes = {}
+  local message = extract_modsecurity_message_string(orig_string)
+  local bracketed = extract_bracketed_data(orig_string)
+  local trailing = extract_trailing_data(orig_string)
+
+  tableMerge(attributes, {message}, bracketed, trailing)
 
   -- Concatenate attributes into a JSON formatted string for New Relic parsing
-  local json_string = table.concat(attributes, ",")
-
-  -- wrap drupal field in brackets to make it parsable json
-  final_string = "{" .. json_string .. "}"
-  return final_string
+  return "{" .. table.concat(attributes, ",") .. "}"
 end
 
 -- The --luacheck:ignore comment suppresses a warning about setting
@@ -41,9 +67,7 @@ end
 -- (like most scripts, this function is called via fluentbit.conf,
 -- which expects it to be defined in this way)
 function parse_modsecurity_keys (_, timestamp, record) --luacheck: ignore
-  record['test'] = "just show me something bro"
-  if (record["message"] ~= nil) then
-    record['test2'] = "did the check work?"
+  if (record["message"] ~= nil and string.find(string.lower(record["message"]), "modsecurity") ~= nil) then
     record["modsecurity"] = modsecurity_attributes_json_string(record["message"])
   end
    -- 2 leaves timestamp unchanged
